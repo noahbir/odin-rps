29af9f9a7f5a398ba738f428310dd180268f18cb
noahbir <noah.birmanns@gmail.com>
2024-07-25 21:08:25 +0200

    bla bla bla

--- script.js
+++ script.js
@@ -4,33 +4,6 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-function playRound() {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-
-    if (humanChoice == "Wrong input") {
-        console.log("Please choose a valid option: rock, paper, or scissors.");
-        return;
-    }
-
-    if(humanChoice == computerChoice) {
-        return console.log("draw");
-    } else if(humanChoice == "rock" && computerChoice == "scissor") {
-        ++humanScore;
-        return console.log("You point");
-    } else if(humanChoice == "paper" && computerChoice == "rock") {
-        ++humanScore;
-        return console.log("You point");
-    } else if(humanChoice == "scissor" && computerChoice == "paper") {
-        ++humanScore;
-        return console.log("You point");
-    } else {
-        ++computerScore;
-        return console.log("Computer points");
-    }
-}
-
-
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3);
 
@@ -40,31 +13,113 @@ function getComputerChoice() {
         case 1:
             return "paper";
         case 2:
-            return "scissor";
+            return "scissors";
     }
 }
 
-function getHumanChoice() {
-    const validChoices = ["rock", "paper", "scissor"];
-    let choice = prompt("Rock, Paper or Scissor?").trim().toLowerCase();
+function changeComputerChoice(str) {
+    let computerChoice = document.getElementById("computerChoice");
     
-    if(validChoices.includes(choice)) {
-        return choice;
+    if(str == "rock") {
+        computerChoice.src = "rock.jpg";
+    } else if(str == "paper") {
+        computerChoice.src = "paper.webp";
+    } else if(str == "scissors") {
+        computerChoice.src = "scissors.avif";
+    }
+    computerChoice.style.display = "block";
+}
+
+function checkResult(str1, str2) {
+    if(str1 == str2) {
+        updateResult("draw");
+    } else if(str1 == "rock" && str2 == "scissors") {
+        updateResult("You point");
+        ++humanScore;
+    } else if(str1 =="paper" && str2 == "rock") {
+        updateResult("You point");
+        ++humanScore;
+    } else if(str1 == "scissors" && str2 == "paper") {
+        updateResult("You point");
+        ++humanScore;
     } else {
-        return "Wrong input";
+        updateResult("Computer points");
+        ++computerScore;
     }
 }
 
-const btn = document.querySelector("#btn");
-btn.addEventListener("click", function(e) {
-    console.log("print");
-    if(humanScore !== 3 && computerScore !== 3) {
-        playRound();
-    } else if(humanScore == 3) {
-        document.getElementById("result").innerHTML = "You win!";
-   } else {
-        document.getElementById("result").innerHTML = "The computer wins!";
-   }
-   document.getElementById("humanScore").innerHTML = humanScore;
-   document.getElementById("computerScore").innerHTML = computerScore;
-})
+function updateResult(str) {
+    let result = document.getElementById("result");
+    result.innerHTML = str;
+}
+
+function updateScoreHuman() {
+    let scoreHuman = document.getElementById("humanScore");
+    scoreHuman.innerHTML = humanScore;
+}
+
+function updateScoreComputer() {
+    let scoreComputer = document.getElementById("computerScore");
+    scoreComputer.innerHTML = computerScore;
+}
+
+function checkWin() {
+    if(humanScore == 3) {
+        updateResult("You win!");
+        return true;
+    } else if(computerScore == 3) {
+        updateResult("Computer wins!");
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+const btnRock = document.querySelector("#btn-rock");
+btnRock.addEventListener("click", function(e) {
+    const humanChoice = "rock";
+    const computerChoice = getComputerChoice();
+    changeComputerChoice(computerChoice);
+    checkResult(humanChoice, computerChoice);
+    updateScoreComputer();
+    updateScoreHuman();
+    if(checkWin() == true) {
+        computerScore = 0;
+        humanScore = 0;
+        updateScoreComputer();
+        updateScoreHuman();
+    }
+});
+
+const btnScissors = document.querySelector("#btn-scissors");
+btnScissors.addEventListener("click", function(e) {
+    const humanChoice = "scissors";
+    const computerChoice = getComputerChoice();
+    changeComputerChoice(computerChoice);
+    checkResult(humanChoice, computerChoice);
+    updateScoreComputer();
+    updateScoreHuman();
+    if(checkWin() == true) {
+        computerScore = 0;
+        humanScore = 0;
+        updateScoreComputer();
+        updateScoreHuman();
+    }
+});
+
+const btnPaper = document.querySelector("#btn-paper");
+btnPaper.addEventListener("click", function(e) {
+    const humanChoice = "paper";
+    const computerChoice = getComputerChoice();
+    changeComputerChoice(computerChoice);
+    checkResult(humanChoice, computerChoice);
+    updateScoreComputer();
+    updateScoreHuman();
+    if(checkWin() == true) {
+        computerScore = 0;
+        humanScore = 0;
+        updateScoreComputer();
+        updateScoreHuman();
+    }
+});
